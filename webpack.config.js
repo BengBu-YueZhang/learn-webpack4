41975828e05c3a5cc571e6389c3b2f8cc9a1d5e2
BengBu-YueZhang <zhangyue_coder@163.com>
2018-09-28 19:04:14 +0800

    css module

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,28 @@ module.exports = {
   output: {
     filename: '[name].[hash].js',
     path: path.resolve(__dirname, './dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+              modules: true,
+              localIdentName: "[local]___[hash:base64:5]"
+            }
+          },
+          {
+            loader: "less-loader"
+          }
+        ]
+      }
+    ]
   }
 }
