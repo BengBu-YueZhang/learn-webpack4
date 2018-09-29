37f188ddae1b6e39dd929b45ac9cb1a38f268d1b
BengBu-YueZhang <zhangyue_coder@163.com>
2018-09-29 10:50:02 +0800

    base

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,68 @@
-const path = require('path')
+var path = require('path')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var webpack = require('webpack')
 
 module.exports = {
+  target: 'web',
+  mode: 'none',
   entry: {
     main: path.resolve(__dirname, './src')
   },
   output: {
     filename: '[name].[hash].js',
     path: path.resolve(__dirname, './dist')
-  }
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 8080,
+    disableHostCheck: true,
+    historyApiFallback: true,
+    host: '0.0.0.0',
+    hot: true,
+    https: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env'
+            ],
+            plugins: [
+              require('@babel/plugin-syntax-dynamic-import')
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+              modules: true,
+              localIdentName: "[local]___[hash:base64:5]"
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './public/index.html')
+    }),
+    new CleanWebpackPlugin(['dist']),
+    new webpack.HotModuleReplacementPlugin(),
+    new MiniCssExtractPlugin()
+  ]
 }
