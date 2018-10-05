c719a3ad9fac5fa0b85a4565a8f5e27c0734c54a
BengBu-YueZhang <zhangyue_coder@163.com>
2018-10-05 20:00:15 +0800

    base

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,40 @@
-const path = require('path')
+var path = require('path')
+var CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
+  target: 'web',
+
+  mode: 'none',
+
   entry: {
-    main: path.resolve(__dirname, './src')
+    main: path.resolve(__dirname, './src/main.js')
   },
+
   output: {
     filename: '[name].[hash].js',
     path: path.resolve(__dirname, './dist')
-  }
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: []
+          }
+        }
+      }
+    ]
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './public/index.html')
+    })
+  ]
 }
